9a9fc6de5974a97a4dedecb564efc1755b43e61e
Pierre Olivier LePage <lepage.pierreolivier@gmail.com>
2019-09-23 13:54:14 -0400

    Dialog Close Behavior

---
+++ WPF.Utils/Behaviors/DialogAwareClose.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xaml.Behaviors;
+using Prism.Services.Dialogs;
+using System.Windows;
+
+namespace WPF.Utils.Behaviors
+{
+    public class DialogAwareClose : Behavior<Window>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            if (AssociatedObject.DataContext is IDialogAware dialogAware)
+            {
+                dialogAware.RequestClose += OnRequestClose;
+            }
+        }
+
+        private void OnRequestClose(IDialogResult obj)
+        {
+            AssociatedObject.Close();
+        }
+    }
+}
