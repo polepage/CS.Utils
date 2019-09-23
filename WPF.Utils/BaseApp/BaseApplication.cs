79e685b0d989309b3450ca7a56727723f3beccec
Pierre Olivier LePage <lepage.pierreolivier@gmail.com>
2019-09-23 12:44:24 -0400

    Init Libs

---
+++ WPF.Utils/BaseApp/BaseApplication.cs
@@ -0,0 +1,41 @@
+﻿using Prism.Events;
+using Prism.Ioc;
+using Prism.Services.Dialogs;
+using Prism.Unity.Ioc;
+using System.Windows;
+
+namespace WPF.Utils.BaseApp
+{
+    public abstract class BaseApplication: Application
+    {
+        protected IContainerExtension Container { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            RegisterContainer();
+            RegisterComponents();
+            RegisterDialogs();
+            RegisterViewModelLocator();
+        }
+
+        private void RegisterContainer()
+        {
+            Container = new UnityContainerExtension();
+            Container.RegisterInstance(Container);
+        }
+
+        protected virtual void RegisterComponents()
+        {
+            Container.RegisterSingleton<IEventAggregator, EventAggregator>();
+        }
+
+        protected virtual void RegisterDialogs()
+        {
+            Container.Register<IDialogService, DialogService>();
+            //_container.RegisterDialogWindow<DialogHost>();
+        }
+
+        protected abstract void RegisterViewModelLocator();
+    }
+}
