c6246f472878490e3bad59ec6388dc23b679036d
Pierre Olivier LePage <lepage.pierreolivier@gmail.com>
2019-09-23 14:07:44 -0400

    More dialog generics

---
+++ WPF.Utils/Dialogs/DialogExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using Prism.Services.Dialogs;
+using System;
+
+namespace WPF.Utils.Dialogs
+{
+    public static class DialogExtensions
+    {
+        public static void ShowOpenDialog(this IDialogService service, IDialogParameters parameters, Action<IDialogResult> callback)
+        {
+            var openDialog = new OpenFileDialog
+            {
+                Title = parameters.GetValue<string>(DialogParams.Title),
+                Filter = parameters.GetValue<string>(DialogParams.File.Filter),
+                ValidateNames = true
+            };
+
+            var dialogResult = new DialogResult(openDialog.ShowDialog().ToButtonResult());
+            if (dialogResult.Result == ButtonResult.OK)
+            {
+                dialogResult.Parameters.Add(DialogParams.File.Target, openDialog.FileName);
+            }
+
+            callback?.Invoke(dialogResult);
+        }
+
+        public static void ShowSaveDialog(this IDialogService service, IDialogParameters parameters, Action<IDialogResult> callback)
+        {
+            var saveDialog = new SaveFileDialog
+            {
+                Title = parameters.GetValue<string>(DialogParams.Title),
+                Filter = parameters.GetValue<string>(DialogParams.File.Filter),
+                ValidateNames = true
+            };
+
+            var dialogResult = new DialogResult(saveDialog.ShowDialog().ToButtonResult());
+            if (dialogResult.Result == ButtonResult.OK)
+            {
+                dialogResult.Parameters.Add(DialogParams.File.Target, saveDialog.FileName);
+            }
+
+            callback?.Invoke(dialogResult);
+        }
+
+        public static ButtonResult ToButtonResult(this bool? result)
+        {
+            return result.GetValueOrDefault() ? ButtonResult.OK : ButtonResult.Cancel;
+        }
+    }
+}
---
+++ WPF.Utils/Dialogs/DialogParams.cs
@@ -0,0 +1,13 @@
+﻿namespace WPF.Utils.Dialogs
+{
+    public static class DialogParams
+    {
+        public static readonly string Title = "Title";
+
+        public static class File
+        {
+            public static readonly string Filter = "Filter";
+            public static readonly string Target = "Target";
+        }
+    }
+}
