c392bc7dd58e70778d38c60bceea6b5865a3ae72
Pierre Olivier LePage <lepage.pierreolivier@gmail.com>
2019-09-23 13:45:20 -0400

    Single Sub event and Base dialog VM

---
+++ CS.Utils/Events/SingleSubEvent.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Events;
+using System;
+
+namespace CS.Utils.Events
+{
+    public class SingleSubEvent : PubSubEvent
+    {
+        protected override SubscriptionToken InternalSubscribe(IEventSubscription eventSubscription)
+        {
+            if (Subscriptions.Count > 0)
+            {
+                throw new InvalidOperationException("SingleSubEvent supports only one subscriber");
+            }
+
+            return base.InternalSubscribe(eventSubscription);
+        }
+    }
+
+    public class SingleSubEvent<TPayload> : PubSubEvent<TPayload>
+    {
+        protected override SubscriptionToken InternalSubscribe(IEventSubscription eventSubscription)
+        {
+            if (Subscriptions.Count > 0)
+            {
+                throw new InvalidOperationException("SingleSubEvent<TPayload> supports only one subscriber");
+            }
+
+            return base.InternalSubscribe(eventSubscription);
+        }
+    }
+}
