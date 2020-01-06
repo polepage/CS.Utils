aefe1f5e613f4ba815dceeda9b2804d45531436b
Pierre Olivier LePage <plepage@lti.ca>
2020-01-05 23:56:41 -0500

    CS.Utils.Unity to back unity without WPF

---
+++ CS.Utils.Unity/PrismIocExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Ioc;
+using Unity;
+
+namespace CS.Utils.Unity
+{
+    /// <summary>
+    /// This class is copied from Prism.Unity.Ioc to be used without the Prism.WPF package.
+    /// </summary>
+    public static class PrismIocExtensions
+    {
+        public static IUnityContainer GetContainer(this IContainerProvider containerProvider)
+        {
+            return ((IContainerExtension<IUnityContainer>)containerProvider).Instance;
+        }
+
+        public static IUnityContainer GetContainer(this IContainerRegistry containerRegistry)
+        {
+            return ((IContainerExtension<IUnityContainer>)containerRegistry).Instance;
+        }
+    }
+}
