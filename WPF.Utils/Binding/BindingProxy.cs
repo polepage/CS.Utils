3c08f7cc8891229a266e3b8b2decdadb64b1d6f2
Pierre Olivier LePage <lepage.pierreolivier@gmail.com>
2020-01-10 16:14:57 -0500

    Added Binding Proxy

---
+++ WPF.Utils/Binding/BindingProxy.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace WPF.Utils.Binding
+{
+    public class BindingProxy : Freezable
+    {
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register("Data",
+                                        typeof(object),
+                                        typeof(BindingProxy),
+                                        new PropertyMetadata());
+
+        public object Data
+        {
+            get => GetValue(DataProperty);
+            set => SetValue(DataProperty, value);
+        }
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
+    }
+}
