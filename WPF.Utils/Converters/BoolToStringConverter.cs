c45221dce4ae6b4abae8bf5765f4a90fa71a7ea0
Pierre Olivier LePage <lepage.pierreolivier@gmail.com>
2020-01-23 13:35:09 -0500

    WPF Helpers

---
+++ WPF.Utils/Converters/BoolToStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WPF.Utils.Converters
+{
+    public class BoolToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool display && display)
+            {
+                return parameter;
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.Equals(parameter);
+        }
+    }
+}
---
+++ WPF.Utils/Markup/BoolExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace WPF.Utils.Markup
+{
+    public class BoolExtension : MarkupExtension
+    {
+        public BoolExtension() { }
+
+        public BoolExtension(bool value)
+        {
+            Value = value;
+        }
+
+        public bool Value { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Value;
+        }
+    }
+}
