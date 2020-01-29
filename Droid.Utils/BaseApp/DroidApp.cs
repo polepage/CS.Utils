67fb3d8d93f51abc129437a177d23e7db2af17b7
Pierre Olivier LePage <lepage.pierreolivier@gmail.com>
2020-01-29 15:37:52 -0500

    Update Prism version + DroidApp

---
+++ Droid.Utils/BaseApp/DroidApp.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Runtime;
+using CS.Utils.Unity;
+using Prism.Ioc;
+using System;
+using Unity;
+
+namespace Droid.Utils.BaseApp
+{
+    public abstract class DroidApp: Application
+    {
+        public DroidApp(IntPtr handle, JniHandleOwnership transfer) : base(handle, transfer) { }
+
+        protected static IContainerExtension ContainerExtension { get; private set; }
+        public static IContainerProvider Container => ContainerExtension;
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            RegisterContainer();
+            RegisterComponents();
+        }
+
+        public override void OnTerminate()
+        {
+            base.OnTerminate();
+            Container?.GetContainer()?.Dispose();
+        }
+
+        protected virtual void RegisterComponents() { }
+
+        private void RegisterContainer()
+        {
+            IUnityContainer unity = new UnityContainer();
+            // Load file?
+
+            ContainerExtension = new UnityContainerExtension(unity);
+            ContainerExtension.RegisterInstance(ContainerExtension);
+        }
+    }
+}
