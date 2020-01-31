5239516d34d6b37ebf17246f883fa797cff6ff87
Pierre Olivier LePage <lepage.pierreolivier@gmail.com>
2020-01-31 15:29:51 -0500

    Updated BaseApplication to support RegionNavigation

--- WPF.Utils/BaseApp/BaseApplication.cs
+++ WPF.Utils/BaseApp/BaseApplication.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.Practices.Unity.Configuration;
+﻿using CommonServiceLocator;
+using Microsoft.Practices.Unity.Configuration;
 using Prism.Events;
 using Prism.Ioc;
+using Prism.Regions;
+using Prism.Regions.Behaviors;
 using Prism.Services.Dialogs;
 using Prism.Unity;
 using Prism.Unity.Ioc;
+using Prism.Unity.Regions;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using Unity;
 using WPF.Utils.Dialogs;
 
@@ -18,10 +24,16 @@ namespace WPF.Utils.BaseApp
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            RegisterContainer();
+            ConfigureContainer();
+            ConfigureServiceLocator();
+
             RegisterComponents();
-            RegisterDialogs();
             RegisterViewModelLocator();
+
+            ConfigureDialogs();
+            ConfigureNavigation();
+
+            ConfigureMainWindow();
         }
 
         protected override void OnExit(ExitEventArgs e)
@@ -30,7 +42,7 @@ namespace WPF.Utils.BaseApp
             base.OnExit(e);
         }
 
-        private void RegisterContainer()
+        private void ConfigureContainer()
         {
             IUnityContainer unity = new UnityContainer();
             if (LoadExternalConfiguration)
@@ -42,17 +54,82 @@ namespace WPF.Utils.BaseApp
             Container.RegisterInstance(Container);
         }
 
+        private void ConfigureServiceLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => Container.Resolve<IServiceLocator>());
+        }
+
+        private void ConfigureMainWindow()
+        {
+            Window window = CreateMainWindow();
+            
+            if (Container.IsRegistered<IRegionManager>())
+            {
+                RegionManager.SetRegionManager(window, Container.Resolve<IRegionManager>());
+                RegionManager.UpdateRegions();
+            }
+
+            MainWindow = window;
+            MainWindow?.Show();
+        }
+
         protected virtual void RegisterComponents()
         {
             Container.RegisterSingleton<IEventAggregator, EventAggregator>();
-        }
-
-        protected virtual void RegisterDialogs()
-        {
-            Container.Register<IDialogService, DialogService>();
-            Container.RegisterDialogWindow<DialogHost>();
+            Container.RegisterSingleton<IServiceLocator, UnityServiceLocatorAdapter>();
         }
 
         protected abstract void RegisterViewModelLocator();
+
+        protected abstract Window CreateMainWindow();
+
+        protected virtual void ConfigureDialogs()
+        {
+            Container.RegisterSingleton<IDialogService, DialogService>();
+            Container.RegisterDialogWindow<DialogHost>();
+
+            RegisterDialogs();
+        }
+
+        protected virtual void RegisterDialogs() { }
+
+        protected virtual void ConfigureNavigation()
+        {
+            Container.RegisterSingleton<IRegionManager, RegionManager>();
+            Container.RegisterSingleton<RegionAdapterMappings>();
+            Container.RegisterSingleton<IRegionBehaviorFactory, RegionBehaviorFactory>();
+            Container.RegisterSingleton<IRegionViewRegistry, RegionViewRegistry>();
+            Container.RegisterSingleton<IRegionNavigationContentLoader, UnityRegionNavigationContentLoader>();
+            Container.Register<IRegionNavigationJournalEntry, RegionNavigationJournalEntry>();
+            Container.Register<IRegionNavigationJournal, RegionNavigationJournal>();
+            Container.Register<IRegionNavigationService, RegionNavigationService>();
+
+            RegisterNavigationMappings();
+            RegisterNavigationBehaviors();
+            RegisterForNavigation();
+        }
+
+        protected virtual void RegisterForNavigation() { }
+
+        protected virtual void RegisterNavigationMappings()
+        {
+            var mappings = Container.Resolve<RegionAdapterMappings>();
+            mappings.RegisterMapping(typeof(Selector), Container.Resolve<SelectorRegionAdapter>());
+            mappings.RegisterMapping(typeof(ItemsControl), Container.Resolve<ItemsControlRegionAdapter>());
+            mappings.RegisterMapping(typeof(ContentControl), Container.Resolve<ContentControlRegionAdapter>());
+        }
+
+        protected virtual void RegisterNavigationBehaviors()
+        {
+            var behaviorFactory = Container.Resolve<IRegionBehaviorFactory>();
+            behaviorFactory.AddIfMissing(BindRegionContextToDependencyObjectBehavior.BehaviorKey, typeof(BindRegionContextToDependencyObjectBehavior));
+            behaviorFactory.AddIfMissing(RegionActiveAwareBehavior.BehaviorKey, typeof(RegionActiveAwareBehavior));
+            behaviorFactory.AddIfMissing(SyncRegionContextWithHostBehavior.BehaviorKey, typeof(SyncRegionContextWithHostBehavior));
+            behaviorFactory.AddIfMissing(RegionManagerRegistrationBehavior.BehaviorKey, typeof(RegionManagerRegistrationBehavior));
+            behaviorFactory.AddIfMissing(RegionMemberLifetimeBehavior.BehaviorKey, typeof(RegionMemberLifetimeBehavior));
+            behaviorFactory.AddIfMissing(ClearChildViewsRegionBehavior.BehaviorKey, typeof(ClearChildViewsRegionBehavior));
+            behaviorFactory.AddIfMissing(AutoPopulateRegionBehavior.BehaviorKey, typeof(AutoPopulateRegionBehavior));
+            behaviorFactory.AddIfMissing(IDestructibleRegionBehavior.BehaviorKey, typeof(IDestructibleRegionBehavior));
+        }
     }
 }
