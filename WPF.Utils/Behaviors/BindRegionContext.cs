b22c5df9ae5e038d92546e47d9298f2d10f01bed
Pierre Olivier LePage <plepage@lti.ca>
2020-02-01 17:13:32 -0500

    RegionContextBehaviors

---
+++ WPF.Utils/Behaviors/BindRegionContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xaml.Behaviors;
+using Prism.Common;
+using Prism.Regions;
+using System.ComponentModel;
+using System.Windows;
+
+namespace WPF.Utils.Behaviors
+{
+    public class BindRegionContext: Behavior<DependencyObject>
+    {
+        private ObservableObject<object> _context;
+
+        public static readonly DependencyProperty ContextProperty =
+            DependencyProperty.Register("Context",
+                                        typeof(object),
+                                        typeof(BindRegionContext));
+
+        public object Context
+        {
+            get => GetValue(ContextProperty);
+            set => SetValue(ContextProperty, value);
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            _context = RegionContext.GetObservableContext(AssociatedObject);
+            _context.PropertyChanged += ContextChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            if (_context != null)
+            {
+                _context.PropertyChanged -= ContextChanged;
+            }
+        }
+
+        private void ContextChanged(object sender, PropertyChangedEventArgs e)
+        {
+            Context = _context.Value;
+        }
+    }
+}
---
+++ WPF.Utils/Behaviors/RegionContextCommand.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xaml.Behaviors;
+using Prism.Common;
+using Prism.Regions;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPF.Utils.Behaviors
+{
+    public class RegionContextCommand : Behavior<DependencyObject>
+    {
+        private ObservableObject<object> _context;
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command",
+                                        typeof(ICommand),
+                                        typeof(RegionContextCommand),
+                                        new PropertyMetadata(CommandChanged));
+
+        public ICommand Command
+        {
+            get => GetValue(CommandProperty) as ICommand;
+            set => SetValue(CommandProperty, value);
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            _context = RegionContext.GetObservableContext(AssociatedObject);
+            _context.PropertyChanged += ContextChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            if (_context != null)
+            {
+                _context.PropertyChanged -= ContextChanged;
+            }
+        }
+
+        private static void CommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as RegionContextCommand)?.Notify();
+        }
+
+        private void ContextChanged(object sender, PropertyChangedEventArgs e)
+        {
+            Notify();
+        }
+
+        private void Notify()
+        {
+            Command?.Execute(_context?.Value);
+        }
+    }
+}
