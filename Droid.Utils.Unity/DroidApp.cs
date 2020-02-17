c199bb0498068c3f6c539f188ddc6b88d49d18f8
Pierre Olivier LePage <plepage@lti.ca>
2020-02-16 19:31:29 -0500

    Base DroidApp

--- Droid.Utils.Unity/DroidApp.cs
+++ Droid.Utils.Unity/DroidApp.cs
@@ -1,8 +1,25 @@
-﻿using System;
+﻿using Android.Runtime;
+using CommonServiceLocator;
+using CS.Utils.Unity;
+using Droid.Utils.BaseApp;
+using Prism.Ioc;
+using System;
 
 namespace Droid.Utils.Unity
 {
-    public class DroidApp
+    public abstract class DroidApp : DroidAppBase
     {
+        public DroidApp(IntPtr handle, JniHandleOwnership transfer) : base(handle, transfer) { }
+
+        protected override IContainerExtension CreateContainerExtension()
+        {
+            return new UnityContainerExtension();
+        }
+
+        protected override void RegisterRequiredTypes(IContainerRegistry containerRegistry)
+        {
+            base.RegisterRequiredTypes(containerRegistry);
+            containerRegistry.RegisterSingleton<IServiceLocator, UnityServiceLocatorAdapter>();
+        }
     }
 }
