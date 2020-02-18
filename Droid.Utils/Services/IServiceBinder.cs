780acddf0ccf72cda18f48b641b2a43c2a67b6a6
Pierre Olivier LePage <lepage.pierreolivier@gmail.com>
2020-02-18 14:44:04 -0500

    ServiceBinder for Droid

---
+++ Droid.Utils/Services/IServiceBinder.cs
@@ -0,0 +1,9 @@
+﻿using Android.OS;
+
+namespace Droid.Utils.Services
+{
+    public interface IServiceBinder<T>: IBinder
+    {
+        T Service { get; }
+    }
+}
---
+++ Droid.Utils/Services/ServiceBinder.cs
@@ -0,0 +1,14 @@
+﻿using Android.OS;
+
+namespace Droid.Utils.Services
+{
+    public class ServiceBinder<T> : Binder, IServiceBinder<T>
+    {
+        public ServiceBinder(T service)
+        {
+            Service = service;
+        }
+
+        public T Service { get; }
+    }
+}
