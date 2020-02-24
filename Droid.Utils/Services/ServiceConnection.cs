3120a322440f6f95f321f7c5b2fde1d23894a8eb
Pierre Olivier LePage <lepage.pierreolivier@gmail.com>
2020-02-24 14:12:38 -0500

    Service Connection

---
+++ Droid.Utils/Services/ServiceConnection.cs
@@ -0,0 +1,27 @@
+﻿using Android.Content;
+using Android.OS;
+using System;
+
+namespace Droid.Utils.Services
+{
+    // T is the service interface
+    public class ServiceConnection<T> : Java.Lang.Object, IServiceConnection
+        where T: class
+    {
+        public event Action Connected;
+        public event Action Disconnected;
+
+        public void OnServiceConnected(ComponentName name, IBinder service)
+        {
+            Service = (service as IServiceBinder<T>)?.Service;
+            Connected?.Invoke();
+        }
+
+        public void OnServiceDisconnected(ComponentName name)
+        {
+            Disconnected?.Invoke();
+        }
+
+        public T Service { get; private set; }
+    }
+}
