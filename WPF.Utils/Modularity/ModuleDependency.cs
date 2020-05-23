316251e877f0970306176f93106cac13cbf79cca
Pierre Olivier LePage <lepage.pierreolivier@gmail.com>
2020-05-22 22:19:13 -0400

    Helper for Module Dependencies in WPF

---
+++ WPF.Utils/Modularity/ModuleDependency.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Modularity;
+using System.Linq;
+using System.Reflection;
+
+namespace WPF.Utils.Modularity
+{
+    /// <summary>
+    /// Reader for module dependency attribute for the WPF case where dependencies are manually added.
+    /// </summary>
+    public static class ModuleDependency
+    {
+        public static string[] GetDependencies<T>()
+        {
+            return typeof(T)
+                .GetCustomAttributes<ModuleDependencyAttribute>()
+                .Select(dependency => dependency.ModuleName)
+                .ToArray();
+        }
+    }
+}
