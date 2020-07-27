25f3ae80578597081930a8c34ae232d258539ac6
Pierre Olivier LePage <plepage@lti.ca>
2020-07-27 18:04:22 -0400

    Added ArrayAdapter enumerator

---
+++ Droid.Utils/ArrayAdapterExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Android.Widget;
+
+namespace Droid.Utils
+{
+    public static class ArrayAdapterExtensions
+    {
+        public static IEnumerable<T> EnumerateAdapter<T>(this ArrayAdapter<T> adapter)
+        {
+            for (int i = 0; i < adapter.Count; i++)
+            {
+                yield return adapter.GetItem(i);
+            }
+        }
+    }
+}
