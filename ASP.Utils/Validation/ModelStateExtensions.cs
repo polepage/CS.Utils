0b5a669fac0da9658434b4c8035336eaaac01324
Pierre Olivier LePage <plepage@lti.ca>
2020-08-13 23:52:21 -0400

    ModelState Extensions

---
+++ ASP.Utils/Validation/ModelStateExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Linq;
+
+namespace ASP.Utils.Validation
+{
+    public static class ModelStateExtensions
+    {
+        public static bool IsValidPartial(this ModelStateDictionary modelState, params string[] keys)
+        {
+            return keys.All(k => modelState.GetValidationState(k) == ModelValidationState.Valid);
+        }
+    }
+}
