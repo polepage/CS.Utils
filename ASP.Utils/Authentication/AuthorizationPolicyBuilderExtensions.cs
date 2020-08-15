50f308d9be5c9a19832041f8996ba0f5aab63860
Pierre Olivier LePage <plepage@lti.ca>
2020-08-15 16:35:50 -0400

    Added extensions classes in ASP.Utils

---
+++ ASP.Utils/Authentication/AuthorizationPolicyBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+
+namespace ASP.Utils.Authentication
+{
+    public static class AuthorizationPolicyBuilderExtensions
+    {
+        public static AuthorizationPolicyBuilder RequireRole<T>(this AuthorizationPolicyBuilder policy, params T[] roles)
+        {
+            return policy.RequireRole(roles.Select(r => r.ToString()));
+        }
+    }
+}
---
+++ ASP.Utils/Authentication/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System.Security.Claims;
+
+namespace ASP.Utils.Authentication
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static bool IsInRole<T>(this ClaimsPrincipal user, params T[] roles)
+        {
+            return roles.Any(r => user.IsInRole(r.ToString()));
+        }
+    }
+}
