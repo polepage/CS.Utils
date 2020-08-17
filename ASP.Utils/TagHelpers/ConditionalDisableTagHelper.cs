d8c14ab2ce5a956d8fc184eb96fc187df99fe28e
Pierre Olivier LePage <plepage@lti.ca>
2020-08-16 20:53:48 -0400

    Tag Helper to add disabled in tags

---
+++ ASP.Utils/TagHelpers/ConditionalDisableTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ASP.Utils.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "conditional-disable")]
+    public class ConditionalDisableTagHelper: TagHelper
+    {
+        public bool ConditionalDisable { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (ConditionalDisable)
+            {
+                output.Attributes.SetAttribute("disabled", "");
+            }
+        }
+    }
+}
